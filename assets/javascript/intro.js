c95619c007c1ed2ee8f03d1cad11bc6acebef469
Veritosvb <vero.velez@gmail.com>
2018-05-12 08:26:56 -0500

    Assigns characters in correct place

--- assets/javascript/intro.js
+++ assets/javascript/intro.js
@@ -1,49 +1,92 @@
 
 var selectedCharacter;
-var yourCharacter = true;;
+var selectedDefender;
+
+var yourDefender = false;
+var yourAttacker = true;
+var arrayWhoishere = [{name:"darth-maul", hp:123,role:"enemy"},{name:"luke-skywalker",hp:89,role:"enemy"},{name:"darth-sidious",hp:30,role:"enemy"},{name:"obi-wan",hp:120,role:"enemy"}];
 
 
 
 $(document).ready(function() {
+    $("#darth-maul").on("click", function(){
     
+    });   
 
 
 $("#darth-maul").on("click", function(){
     setAttacker(this);
-
+    setDefender(this);
 });
 
 $("#darth-sidious").on("click", function(){
     setAttacker(this);
+    setDefender(this);
 });
 
 $("#luke-skywalker").on("click", function(){
     setAttacker(this);
+    setDefender(this);
 });
 
 $("#obi-wan").on("click", function(){
     setAttacker(this);
+    setDefender(this);
 });
 
 $("#attack").on("click", function(){
-    console.log("clicked");
+   if(!yourAttacker && !yourDefender){
+        console.log("ready to attack");
+        attack();
+   }
 });
 
 });
+
 
 
 function setAttacker(event){
-    if(yourCharacter){
-        yourCharacter =false;
-        selectedCharacter = event.id;
-        $("#" + selectedCharacter).appendTo("#selectedPlayer")
+    if(yourAttacker){
+        yourAttacker =false;
+        for(var i = 0; i < arrayWhoishere.length; i++){
+            if(arrayWhoishere[i].name ==event.id){
+                selectedCharacter = arrayWhoishere[i];
+                arrayWhoishere[i].role = "attack";
+                $("#" + event.id).attr( "style", "background-color: green");
+                $("#" + event.id).appendTo("#selectedPlayer");
+                $("#apt").remove();
+            }else if(arrayWhoishere[i].role =="enemy"){
+                $("#" + arrayWhoishere[i].name).attr( "style", "background-color: red");
+                $("#" + arrayWhoishere[i].name).appendTo("#enemiesAvailable");
+            }
+        }
+        yourDefender=true;
     }
 }
 
-function randomPower(){
-
+function setDefender(event){
+    if(!yourAttacker && yourDefender && selectedCharacter != event.id){
+        yourDefender=false;
+        for(var i = 0; i < arrayWhoishere.length; i++){
+            if(arrayWhoishere[i].name ==event.id){
+                selectedDefender = arrayWhoishere[i];
+                arrayWhoishere[i].role = "defend";
+                $("#" + event.id).attr( "style", "background-color: black; color:white" );
+                $("#" + event.id).appendTo("#Defender");
+            }
+         }
+    }
 }
 
-function myCharacter(){
+function randomToDefender(){
+var damage = 0;
+return damage;
+}
+
+function attack(){
+var g = selectedDefender.hp;
+selectedDefender.hp = selectedDefender.hp + randomToDefender();
+$("#messagesFight").text("You attacked " + selectedDefender.name + " for " + randomToDefender() + " damage <br> ")
+$("#messagesFight").text(selectedDefender.name + "attacked you back for " + randomToDefender())
 
 }
